90ec05ed44cd6a12bb1447bbf64ca947429db809
chao <zhaohc53@gmali.com>
2018-09-06 18:05:09 +0800

    first check in

---
+++ grid_ui.cpp
@@ -0,0 +1,6 @@
+#include "grid_ui.h"
+
+grid_ui::grid_ui(QWidget *parent) : QWidget(parent)
+{
+
+}
---
+++ grid_ui.h
@@ -0,0 +1,17 @@
+#ifndef GRID_UI_H
+#define GRID_UI_H
+
+#include <QWidget>
+
+class grid_ui : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit grid_ui(QWidget *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // GRID_UI_H
